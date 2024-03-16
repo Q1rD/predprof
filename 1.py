c8a8dfb6e54ea6bc2011adba53ddf07d07ac966d
kr7a-J <91874253+Q1rD@users.noreply.github.com>
2024-03-16 12:19:53 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,15 @@
+import csv
+
+
+def main() -> None:
+    with open("magical.csv") as input_file:
+        reader = list(csv.reader(input_file, delimiter=","))
+        for i in reader[1:]:
+            row = f"{i[0]} в запасах еще есть - {i[1]}\n"
+            print(row)
+            with open('magicaPotions.txt', 'a') as output:
+                output.write(row)
+
+
+
+main()
