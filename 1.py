ad1fba61f5100b84d586a3e0e40f967046726e72
kr7a-J <91874253+Q1rD@users.noreply.github.com>
2024-03-16 12:32:19 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,13 +2,11 @@ import csv
 
 
 def main() -> None:
-    """
-    основная функция программы
-    :return: None
-    """
     with open("magical.csv") as input_file:
         reader = list(csv.reader(input_file, delimiter=","))
         for i in reader[1:]:
+            if i[1] == -1:
+                i[1] = 0
             row = f"{i[0]} в запасах еще есть - {i[1]}\n"
             print(row)
             with open('magicaPotions.txt', 'a') as output:
